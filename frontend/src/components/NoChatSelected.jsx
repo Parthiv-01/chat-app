573f10a09a6eb58f0bfa65b60715239285128f44
Parthiv Sadhukhan <parthivsadhukhan@gmail.com>
2025-04-07 15:15:34 +0530

    Update NoChatSelected.jsx

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -1,4 +1,4 @@
-import { MessageSquare } from "lucide-react";
+import { MessageSquareShare } from "lucide-react";
 
 const NoChatSelected = () => {
   return (
@@ -11,13 +11,13 @@ const NoChatSelected = () => {
               className="w-16 h-16 rounded-2xl bg-primary/10 flex items-center
              justify-center animate-bounce"
             >
-              <MessageSquare className="w-8 h-8 text-primary " />
+              <MessageSquareShare className="w-8 h-8 text-primary " />
             </div>
           </div>
         </div>
 
         {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
+        <h2 className="text-2xl font-bold">Welcome to Chatter!</h2>
         <p className="text-base-content/60">
           Select a conversation from the sidebar to start chatting
         </p>
